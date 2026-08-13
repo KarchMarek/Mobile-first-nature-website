11483e2432bf2691329e0dc6648d79054bdbc049
KarchMarek <karch.marek76@gmail.com>
2026-08-13 06:30:02 +0200

    Add smooth scrolling and section highlighting

--- js/script.js
+++ js/script.js
@@ -1,9 +1,34 @@
 const nav = document.querySelector(".main-nav");
 const hamburger = document.querySelector(".menu-toggle");
-const iconHamburger = document.querySelector(".menu-toggle i")
+const iconHamburger = document.querySelector(".menu-toggle i");
+
+const navLinks = document.querySelectorAll(".main-nav a");
 
 hamburger.addEventListener("click", function(){
     nav.classList.toggle("active");
     iconHamburger.classList.toggle("fa-bars");
     iconHamburger.classList.toggle("fa-xmark");
+});
+
+navLinks.forEach(function(link){
+    link.addEventListener("click", function(event){
+
+        event.preventDefault();
+
+        const target = document.querySelector(link.getAttribute("href"));
+
+        target.scrollIntoView({
+            behavior: "smooth"
+        });
+
+        target.classList.add("highlight");
+
+        setTimeout(function(){
+            target.classList.remove("highlight");
+        }, 1000);
+
+        nav.classList.remove("active");
+        iconHamburger.classList.remove("fa-xmark");
+        iconHamburger.classList.add("fa-bars");
+    });
 });
